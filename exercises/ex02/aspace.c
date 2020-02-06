7bce6badcbf7d9edbdb9df45bda3a747fb5d37ea
SamYoung20 <samantha.young@students.olin.edu>
2020-02-05 23:48:36 -0500

    Checking in Address space and stack allocated data

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -9,18 +9,38 @@ License: GNU GPLv3
 #include <stdlib.h>
 
 int var1;
-
+void print_addr(int variable){
+  printf ("Address of local variable is %p\n", &variable);
+}
 int main ()
 {
     int var2 = 5;
+    int var3 = 7;
     void *p = malloc(128);
     char *s = "Hello, World";
+    void *x = malloc(128);
+    void *y = malloc(8);
+    void *z = malloc(8);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
-    printf ("Address of var2 is %p\n", &var2);
+    print_addr(var3);
+  //  printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
-    printf ("s points to %p\n", s);
+    printf ("y points to %p\n", y);
+    printf ("z points to %p\n", z);
 
     return 0;
 }
+
+/*
+4. Heap grow upward.
+5. Stack grows downward.
+6. Random number 8. I allocated two mallocs that wer
+each 8 bytes big. The two address of these locations
+were 32 bits apart. 0x20.
+
+
+
+4/
+*/
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -26,7 +26,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    //printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -45,3 +45,34 @@ int main()
 
     return 0;
 }
+/*
+### Stack allocated data
+
+1.  Read `stack.c`, which should be in this directory.  What is it
+intended to do?  What would the output be if the program worked as
+expected?
+
+The program inteds to make an array of size 5 filled with the number 42
+in each element of the array. It then intends to print the array and change the
+values to be 1 2 3 4 5.
+
+Finally it is supposed to print the array.
+
+2. You get a warning in the function "foo"
+    warning: function returns address of local variable [-Wreturn-local-addr]
+     return array;
+     This function returns the address of a local variable. Local variables are
+     created in the stack and destroyed when the program goes out of scope. This
+     function returns a pointer to an invalid memory location.
+
+3. When running the code I got a segmentation fault. This means I am trying to
+access data I don't have access to.
+4. When commenting out the print statements in foo() and bar() I still get a
+seg fault. I remove the seg fault by commenting out the final print statement in
+main(), rintf("%d\n", array[i]); This is because we are trying to access array.
+Array was created in foo and then destroyed so the pointer doesn't point to a
+valid place in memory. A way to fix this would be to pass a variable that can
+exist between the scopes of these funcitons or not point to stack allocated data. 
+
+
+*/
