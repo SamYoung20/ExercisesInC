303b705cc6848f39434154be81febe448c41ec9c
SamYoung20 <samantha.young@students.olin.edu>
2020-03-04 00:47:40 -0500

    adding find_track_oo.c

--- exercises/ex05/find_track_oo.c
+++ exercises/ex05/find_track_oo.c
@@ -21,6 +21,7 @@ char tracks[][80] = {
 
 
 typedef regex_t Regex;
+regex_t regex;
 
 
 /* Returns a new Regex that matches the given pattern.
@@ -30,8 +31,9 @@ typedef regex_t Regex;
 * returns: new Regex
 */
 Regex *make_regex(char *pattern, int flags) {
-    // FILL THIS IN!
-    return NULL;
+    int ret;
+    ret = regcomp(&regex, pattern, flags);
+    return &regex;
 }
 
 /* Checks whether a regex matches a string.
@@ -41,16 +43,24 @@ Regex *make_regex(char *pattern, int flags) {
 * returns: 1 if there's a match, 0 otherwise
 */
 int regex_match(Regex *regex, char *s) {
-    // FILL THIS IN!
+  int ret = 0;
+  ret = regexec(regex, s, 0, NULL, 0);
+  if (!ret) {
+    return 1;
+  }
+  else {
     return 0;
+  }
 }
+
+
 
 /* Frees a Regex.
 *
 * regex: Regex pointer
 */
 void regex_free(Regex *regex) {
-    // FILL THIS IN!
+    regfree(regex);
 }
 
 
